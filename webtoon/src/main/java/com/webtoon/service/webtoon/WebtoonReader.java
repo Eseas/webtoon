0b69a4e12a2cb2bb61aaea0f5e579e7d939fd96f
Eseas <8snlf9@gmail.com>
2025-03-12 21:55:44 +0900

    feature : webtoon recommend

--- webtoon/src/main/java/com/webtoon/service/webtoon/WebtoonReader.java
+++ webtoon/src/main/java/com/webtoon/service/webtoon/WebtoonReader.java
@@ -4,6 +4,10 @@ import com.webtoon.domain.entity.Webtoon;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface WebtoonReader {
+    Webtoon read(Long id);
     Page<Webtoon> readPage(Pageable pageable);
+    List<Webtoon> readAll();
 }
